44e48e84cb403451c9048ef824b4a2e043b9685f
OcarinhaOfTime <vagnerseibert@gmail.com>
2018-02-19 17:08:20 -0300

    Criado botão de teste para newduardo.

---
+++ WarDinos-Unity/WarDinos/Assets/Scripts/HUD/HUDMatch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDMatch : MonoBehaviour {
+    public Button voltar;
+    
+	void Start () {
+        voltar.onClick.AddListener(() => print("bite my shinny metal ass!"));
+    }
+}
